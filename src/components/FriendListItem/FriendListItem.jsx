567c161890e780bd13e899e739cdc29d766e8e09
ShulgaRuslan <shulgar1988@gmail.com>
2022-09-21 16:28:35 +0300

    correction
    add Data
    add protoTypes in Profile.jsx
    protoTypes in FriendListItem

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -12,7 +12,7 @@ export const FriendListItem = ({ avatar, name, isOnline }) => {
     );
 };
 
-FriendListItem.protoTypes = {
+FriendListItem.propTypes = {
      avatar: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     isOnline: PropTypes.bool.isRequired,
